44deb3eda45423d23e97e53bb786c51666a55311
Alexander Freas <akfreas@gmail.com>
2022-05-03 19:08:23 +0200

    File decrypting works

--- Shadowpix/Utils/VideoFilesManager.swift
+++ Shadowpix/Utils/VideoFilesManager.swift
@@ -21,9 +21,59 @@ struct VideoFileProcessor {
     let destinationURL: URL
     let key: Array<UInt8>
     
+//    private let header: SecretStream.XChaCha20Poly1305.Header?
     private let sodium = Sodium()
-    private let chunkSize = 1024
     
+    private enum Constants {
+        static let defaultBlockSize: Int = 1024
+    }
+    
+    func chunkedOperation(sourceFileHandle: FileHandle,
+                          destinationFileHandle: FileHandle,
+                          operation: @escaping ([UInt8], Bool) -> Data,
+                          blockSize: Int = Constants.defaultBlockSize,
+                          completion: @escaping (URL?, VideoFilesManagerError?) -> Void) {
+        
+        guard FileManager.default.fileExists(atPath: sourceURL.path) else {
+            completion(nil, .sourceFileAccessError)
+            return
+        }
+            
+        do {
+            
+            guard var data = try sourceFileHandle.read(upToCount: blockSize) else {
+                completion(nil, .sourceFileAccessError)
+                return
+            }
+            var byteArray = [UInt8](repeating: 0, count: data.count)
+            
+            data.copyBytes(to: &byteArray, count: data.count)
+            //optimize var usage in this loop
+            while true {
+                let final = byteArray.count < blockSize
+                let outputData = operation(byteArray, final)
+                try destinationFileHandle.write(contentsOf: outputData)
+               
+                guard let nextChunk = try sourceFileHandle.read(upToCount: blockSize) else {
+                    break
+                }
+                data = nextChunk
+                byteArray = [UInt8](repeating: 0, count: data.count)
+                data.copyBytes(to: &byteArray, count: data.count)
+                
+            }
+            
+            //close outputFileHandle after reading data complete.
+            sourceFileHandle.closeFile()
+            completion(destinationURL, nil)
+            print("File reading complete")
+            
+        } catch let error as NSError {
+            completion(nil, .encryptError)
+            print("Error : \(error.localizedDescription)")
+        }
+
+    }
     
     func encryptVideo(completion: @escaping (URL?, VideoFilesManagerError?) -> Void) {
                 
@@ -32,47 +82,32 @@ struct VideoFileProcessor {
             completion(nil, .keyError)
             return
         }
-        
-        guard FileManager.default.fileExists(atPath: sourceURL.path) else {
-            completion(nil, .sourceFileAccessError)
-            return
-        }
-                        
-            
+        print("encrypting", key, streamEnc.header())
+        //open file for reading.
         do {
-            
-            //open file for reading.
-            let videoSourceFileHandle = try FileHandle(forReadingFrom: sourceURL)
+            let sourceFileHandle = try FileHandle(forReadingFrom: sourceURL)
             FileManager.default.createFile(atPath: destinationURL.path, contents: nil)
-            let encryptedDestinationFileHandle = try FileHandle(forWritingTo: destinationURL)
-            
-            // get the first chunk
-            var data = videoSourceFileHandle.readData(ofLength: chunkSize)
-            var byteArray = [UInt8](repeating: 0, count: data.count)
-            
-            data.copyBytes(to: &byteArray, count: data.count)
-            
-            while !(data.isEmpty) {
+            let destinationFileHandle = try FileHandle(forWritingTo: destinationURL)
+            let header = streamEnc.header()
+            try destinationFileHandle.write(contentsOf: Data(header))
+            var writeBlockSizeOperation: (([UInt8]) -> Void)?
+            writeBlockSizeOperation = { cipherText in
                 
-                let message = streamEnc.push(message: byteArray)!
-                let encryptedData = Data(message)
-                try encryptedDestinationFileHandle.write(contentsOf: encryptedData)
-                
-                
-                data = videoSourceFileHandle.readData(ofLength: chunkSize)
-                byteArray = [UInt8](repeating: 0, count: data.count)
-                data.copyBytes(to: &byteArray, count: data.count)
-                
+                let cipherTextLength = withUnsafeBytes(of: cipherText.count) {
+                        Array($0)
+                    }
+                    try! destinationFileHandle.write(contentsOf: Data(cipherTextLength))
+                    writeBlockSizeOperation = nil
             }
+            chunkedOperation(sourceFileHandle: sourceFileHandle,
+                             destinationFileHandle: destinationFileHandle,
+                             operation: { (bytes, isFinal) in
+                let message = streamEnc.push(message: bytes, tag: isFinal ? .FINAL : .MESSAGE)!
+                writeBlockSizeOperation?(message)
+                return Data(message)
+            }, completion: completion)
+        } catch {
             
-            //close outputFileHandle after reading data complete.
-            videoSourceFileHandle.closeFile()
-            completion(destinationURL, nil)
-            print("File reading complete")
-            
-        } catch let error as NSError {
-            completion(nil, .encryptError)
-            print("Error : \(error.localizedDescription)")
         }
     }
     
@@ -80,6 +115,37 @@ struct VideoFileProcessor {
         
         
         
+        do {
+            let sourceFileHandle = try FileHandle(forReadingFrom: sourceURL)
+            FileManager.default.createFile(atPath: destinationURL.path, contents: nil)
+            let destinationFileHandle = try FileHandle(forWritingTo: destinationURL)
+            let headerBytes = try sourceFileHandle.read(upToCount: 24)
+            var headerBuffer = [UInt8](repeating: 0, count: 24)
+            headerBytes?.copyBytes(to: &headerBuffer, count: 24)
+            print("decrypting", key, headerBuffer)
+            
+            let blockSizeInfo = try sourceFileHandle.read(upToCount: 8)
+            let blockSize: UInt32 = blockSizeInfo!.withUnsafeBytes({ $0.load(as: UInt32.self)
+            })
+            
+            
+            guard let streamDec = sodium.secretStream.xchacha20poly1305.initPull(secretKey: key, header: headerBuffer) else {
+                print("Could not create stream with key")
+                completion(nil, .keyError)
+                return
+            }
+            
+            chunkedOperation(sourceFileHandle: sourceFileHandle,
+                             destinationFileHandle: destinationFileHandle,
+                             operation: { (bytes, isFinal) in
+                let (message, tag) = streamDec.pull(cipherText: bytes)!
+                return Data(message)
+            },
+                             blockSize: Int(blockSize),
+                             completion: completion)
+        } catch {
+            
+        }
     }
     
     private func chunkedRead(operation: @escaping (Data) -> (Data)) {
--- ShadowpixTests/VideoFilesManagerTests.swift
+++ ShadowpixTests/VideoFilesManagerTests.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import XCTest
+import Sodium
 @testable import Shadowpix
 
 class VideoFilesManagerTests: XCTestCase {
@@ -17,10 +18,12 @@ class VideoFilesManagerTests: XCTestCase {
 //        let sourceUrl = TempFilesManager().createTemporaryMovieUrl()
 //        try FileManager.default.copyItem(at: url, to: sourceUrl)
         let destinationUrl = TempFilesManager().createTemporaryMovieUrl()
-        let handler = VideoFileProcessor(sourceURL: sourceUrl, destinationURL: destinationUrl)
+        let key = Sodium().secretStream.xchacha20poly1305.key()
+        let handler = VideoFileProcessor(sourceURL: sourceUrl, destinationURL: destinationUrl, key: key)
         let expectation = expectation(description: "video")
         handler.encryptVideo { (url, error) in
             XCTAssertNil(error)
+            XCTAssertTrue(FileManager.default.fileExists(atPath: url!.path))
             expectation.fulfill()
         }
         
@@ -29,6 +32,34 @@ class VideoFilesManagerTests: XCTestCase {
     
     func testDecryptVideo() throws {
         
+        let sourceUrl = Bundle(for: type(of: self)).url(forResource: "test", withExtension: "mov")!
+//        let sourceUrl = TempFilesManager().createTemporaryMovieUrl()
+//        try FileManager.default.copyItem(at: url, to: sourceUrl)
+        let destinationUrl = TempFilesManager().createTemporaryMovieUrl()
+        let key = Sodium().secretStream.xchacha20poly1305.key()
+        let handler = VideoFileProcessor(sourceURL: sourceUrl, destinationURL: destinationUrl, key: key)
+        let encryptExpectation = expectation(description: "encrypt video")
+        var encryptedURL: URL?
+        handler.encryptVideo { (url, error) in
+            XCTAssertNil(error)
+            encryptedURL = url
+            XCTAssertTrue(FileManager.default.fileExists(atPath: url!.path))
+            encryptExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 10)
+
+        let unwrappedURL = try XCTUnwrap(encryptedURL)
+        print(unwrappedURL)
+        let decryptExpectation = expectation(description: "decrypt video")
+        
+        let unencryptedDestination = TempFilesManager().createTemporaryMovieUrl()
+        let decryptHandler = VideoFileProcessor(sourceURL: unwrappedURL, destinationURL: unencryptedDestination, key: key)
+        decryptHandler.decryptVideo { (url, error) in
+            print("decrypted file:", url!)
+            decryptExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 10)
         
     }
     
