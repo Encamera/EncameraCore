3819c2a1b48baff0e822fcc0d5c9063e796ef7a3
Alexander Freas <akfreas@gmail.com>
2022-06-29 23:16:17 +0200

    Initialize fileAccess on app state

--- Shadowpix/Models/ShadowPixState.swift
+++ Shadowpix/Models/ShadowPixState.swift
@@ -14,7 +14,8 @@ class ShadowPixState: ObservableObject {
     
     
     private(set) var authManager: AuthManager
-    var keyManager: KeyManager!
+    var keyManager: KeyManager
+    var fileAccess: DiskFileAccess<iCloudFilesDirectoryModel>?
     private var cancellables = Set<AnyCancellable>()
     @Published var cameraMode: CameraMode = .photo
     @Published var scannedKey: ImageKey? {
@@ -37,6 +38,13 @@ class ShadowPixState: ObservableObject {
         
         self.authManager = AuthManager()
         self.keyManager = MultipleKeyKeychainManager(isAuthorized: self.authManager.$isAuthorized.eraseToAnyPublisher())
+        self.keyManager.keyPublisher.sink { newKey in
+            guard let key = newKey else {
+                return
+            }
+            self.fileAccess = DiskFileAccess<iCloudFilesDirectoryModel>(key: key)
+        }.store(in: &cancellables)
+        
         NotificationCenter.default
             .publisher(for: UIApplication.didEnterBackgroundNotification)
             .sink { _ in
