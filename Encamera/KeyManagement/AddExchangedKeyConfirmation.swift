9369914e0395e6a3df4831c7f97246650b016093
Alexander Freas <akfreas@gmail.com>
2023-04-26 18:33:38 +0200

    Add save to icloud on exchange key confirmation

--- Encamera/KeyManagement/AddExchangedKeyConfirmation.swift
+++ Encamera/KeyManagement/AddExchangedKeyConfirmation.swift
@@ -13,6 +13,7 @@ class AddExchangedKeyConfirmationViewModel: ObservableObject {
     var key: PrivateKey
     var selectedStorageType: StorageType?
     var keyManager: KeyManager
+    @Published var saveToiCloud: Bool = false
     
     init(key: PrivateKey, keyManager: KeyManager) {
         self.key = key
@@ -23,7 +24,7 @@ class AddExchangedKeyConfirmationViewModel: ObservableObject {
         guard let storageType = selectedStorageType else {
             return
         }
-        try keyManager.save(key: key, storageType: storageType, setNewKeyToCurrent: true)
+        try keyManager.save(key: key, storageType: storageType, setNewKeyToCurrent: true, backupToiCloud: saveToiCloud)
         
     }
     
@@ -39,7 +40,9 @@ struct AddExchangedKeyConfirmation: View {
             
             Text(L10n.keyName(viewModel.key.name))
             Text(L10n.creationDate(DateUtils.dateOnlyString(from: viewModel.key.creationDate)))
-            
+            Toggle(isOn: $viewModel.saveToiCloud) {
+                Text(L10n.saveKeyToICloud)
+            }
             Button {
                 
             } label: {
