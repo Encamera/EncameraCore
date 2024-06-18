0b16552970ab9ce1a15f81da0ae037a0f213585c
Alexander Freas <akfreas@gmail.com>
2024-06-18 11:16:47 +0200

    Add more notifications for carousel: review, reddit

--- Encamera/Components/NotificationCarousel.swift
+++ Encamera/Components/NotificationCarousel.swift
@@ -93,9 +93,17 @@ class NotificationCarouselViewModel: ObservableObject {
             self?.showingWebView = true
         }
         self.notifications = [
-            .init(image: Image("NotificationBanner-ShieldCheck"), titleText: L10n.notificationBannerTitle, bodyText: L10n.notificationBannerBody, buttonText: nil, id: 0),
-            .init(image: Image("Telegram-Logo"), titleText: L10n.telegramGroupJoinTitle, bodyText: L10n.telegramGroupJoinBody, buttonText: L10n.telegramGroupJoinButtonText, buttonUrl: URL(string: "https://t.me/encamera_app")!, id: 1),
-            .init(image: Image("NotificationBanner-Widget"), titleText: L10n.installWidgetTitle, bodyText: L10n.installWidgetBody, buttonText: L10n.installWidgetButtonText, buttonUrl: URL(string: "https://vimeo.com/896507875")!, id: 2, tappedAction: tappedAction),
+            .init(image: Image("NotificationBanner-LeaveReview"), titleText: L10n.NotificationBanner.LeaveAReview.title, bodyText: L10n.NotificationBanner.LeaveAReview.body, buttonText: L10n.leaveAReview, buttonUrl: AskForReviewUtil.reviewURL, id: 1, tappedAction: { _ in
+                Task { @MainActor in
+                    AskForReviewUtil.openAppStoreReview()
+                }
+            }),
+            .init(image: Image("NotificationBanner-Reddit"), titleText: L10n.NotificationBanner.Reddit.title, bodyText: L10n.NotificationBanner.Reddit.body, buttonText: L10n.NotificationBanner.Reddit.button, buttonUrl: URL(string: "https://www.reddit.com/r/encamera/")!, id: 2, tappedAction: { url in
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }),
+            .init(image: Image("Telegram-Logo"), titleText: L10n.telegramGroupJoinTitle, bodyText: L10n.telegramGroupJoinBody, buttonText: L10n.telegramGroupJoinButtonText, buttonUrl: URL(string: "https://t.me/encamera_app")!, id: 3),
+            .init(image: Image("NotificationBanner-Widget"), titleText: L10n.installWidgetTitle, bodyText: L10n.installWidgetBody, buttonText: L10n.installWidgetButtonText, buttonUrl: URL(string: "https://vimeo.com/896507875")!, id: 4, tappedAction: tappedAction),
+
         ].shuffled()
     }
 }
